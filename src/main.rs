88495b0184772005dd21d855edde854b9d8329cd
hujuu <out.of.the.base.sfc@gmail.com>
2022-07-15 04:29:08 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ extern crate csv;
 use csv::Error;
 use std::fs::File;
 use std::io::prelude::*;
+use qrcode_generator::QrCodeEcc;
 
 fn main() -> Result<(), Error> {
-    use qrcode_generator::QrCodeEcc;
     qrcode_generator::to_png_to_file("Hello world!", QrCodeEcc::Medium, 1024, "tmp/file_output.png").unwrap();
     let filename = "sample.csv";
     // --snip--
@@ -16,19 +16,15 @@ fn main() -> Result<(), Error> {
     f.read_to_string(&mut contents)
         // ファイルの読み込み中に問題がありました
         .expect("something went wrong reading the file");
-    
     // テキストは\n{}です
     println!("With text:\n{}", contents);
     
     let mut reader = csv::Reader::from_reader(contents.as_bytes());
     for record in reader.records() {
         let record = record?;
+        let _qr_url = record[1].to_string();
         println!(
-            "In {}, {} built the {} model. It is a {}.",
-            &record[0],
             &record[1],
-            &record[2],
-            &record[3]
         );
     }
     
