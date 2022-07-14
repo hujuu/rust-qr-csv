581a1203ccd39e16ff8cc5eba1da825f041b7787
hujuu <out.of.the.base.sfc@gmail.com>
2022-07-15 04:39:14 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,32 @@ extern crate csv;
 use csv::Error;
 use std::fs::File;
 use std::io::prelude::*;
+use std::time::Instant;
 use qrcode_generator::QrCodeEcc;
 
 fn main() -> Result<(), Error> {
-    qrcode_generator::to_png_to_file("Hello world!", QrCodeEcc::Medium, 1024, "tmp/file_output.png").unwrap();
     let filename = "sample.csv";
-    // --snip--
     println!("In file {}", filename);
     // ファイルが見つかりませんでした
     let mut f = File::open(filename).expect("file not found");
-    
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         // ファイルの読み込み中に問題がありました
         .expect("something went wrong reading the file");
     // テキストは\n{}です
     println!("With text:\n{}", contents);
-    
     let mut reader = csv::Reader::from_reader(contents.as_bytes());
     for record in reader.records() {
         let record = record?;
         let _qr_url = record[1].to_string();
-        println!(
-            &record[1],
-        );
+        let directory = String::from("tmp/qr");
+        let name = &record[0];
+        let output = directory + name;
+        let start = Instant::now();
+        qrcode_generator::to_png_to_file(_qr_url, QrCodeEcc::Medium, 300, output).unwrap();
+        let end = start.elapsed();
+        println!("{}.{:03}秒経過しました。", end.as_secs(), end.subsec_nanos() / 1_000_000);
     }
-    
     Ok(())
 }
 
